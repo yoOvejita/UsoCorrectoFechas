87d370efcc26e74cdc3375c1932d42d426b47a26
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2020-05-27 21:26:19 -0400

    Commit unico

---
+++ FormularioEnvioFecha/Controllers/CuentaController.cs
@@ -0,0 +1,28 @@
+﻿using FormularioEnvioFecha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormularioEnvioFecha.Controllers
+{
+    public class CuentaController : Controller
+    {
+        // GET: Cuenta
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Cuenta cuenta = new Cuenta() {
+                FechaNacimiento = DateTime.Now
+            };
+
+            return View("Index", cuenta);
+        }
+        [HttpPost]
+        public ActionResult Registrar(Cuenta cc) {
+            ViewBag.cuenta = cc;
+            return View("Success");
+        }
+    }
+}
---
+++ FormularioEnvioFecha/Models/Cuenta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormularioEnvioFecha.Models
+{
+    public class Cuenta
+    {
+        public string Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string NombreCompleto { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
